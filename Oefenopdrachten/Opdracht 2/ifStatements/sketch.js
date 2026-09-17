31dc4a60676b666375f4ed23d50a75d90d300ffe
Ferencb09 <Ferenc3417@gmail.com>
2026-09-17 14:50:22 +0200

    V0.1.0

--- Oefenopdrachten/Opdracht 2/ifStatements/sketch.js
+++ Oefenopdrachten/Opdracht 2/ifStatements/sketch.js
@@ -1,5 +1,6 @@
-let a = 1
-let b = 1
+let a;
+let b;
+
 
 function setup() {
   createCanvas(1855, 1000);
@@ -9,11 +10,20 @@ function draw() {
   background(200);
 text(b, 100, 100)
 text(a, 100, 115)
+
+if (b >= a) {
+  text("B is groter dan A", 100, 130)
+} else if(a >= b){
+ text("A is groter dan B", 100, 130)
+} else{
+  text("Bijde A en B zijn even groot", 100, 130)
+}
 }
 
 function keyPressed(){
-  if (key === 'Space') {
-    b = random(0, 100)
-    a = random(0, 100)
+  if (key) {
+    b = round(random(0, 100))
+    a = round(random(0, 100))
   }
+  
 }
