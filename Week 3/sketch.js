e13f214cab50b8809e4032159cf6fb4cebce41fd
Ferencb09 <Ferenc3417@gmail.com>
2026-09-23 12:09:16 +0200

    v0.1.1

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -10,7 +10,10 @@ let target = 180;
 // Hier niet aan zitten!
 
 let backgroundcirclesize = 1500
-let turn = 1
+let turn = 1 // Blue = 1 Red = 2
+
+let quitColor = 200
+let playAgainColor = 200
 
 let block1 = 0
 let block2 = 0
@@ -32,6 +35,11 @@ let b7c = 255
 let b8c = 255
 let b9c = 255
 
+let redwon = 0
+let bluewon = 0
+
+let ongoingGame = 1
+let homeScreen = 1
 
 function setup() {
   createCanvas(1850, 1000);
@@ -40,10 +48,16 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(0)
+   if(redwon == 1 || turn == 2){
   fill(255, 0 ,0)
+  circle(900,500,backgroundcirclesize)
+   }
   circle(900,500,1500)
+  if(bluewon == 1 || turn == 1){
   fill(0,0,255)
   circle(900,500,backgroundcirclesize)
+  }
+ 
   fill(0)
   circle(900,500,1450)
   fill(190)
@@ -78,7 +92,7 @@ function draw() {
   fill(b9c)
   square(1020 , 620, grooteVierkanten, 10)
 
-
+if(ongoingGame == 1) {
   //Eerste Rij
 
   if (mouseX > 620 && mouseX < 800 && mouseY > 220 && mouseY < 400) {
@@ -131,7 +145,18 @@ function draw() {
     b9c = 200
   }
   else{b9c = 255}
-
+}
+else{
+  b1c = 255
+  b2c = 255
+  b3c = 255
+  b4c = 255
+  b5c = 255
+  b6c = 255
+  b7c = 255
+  b8c = 255
+  b9c = 255
+}
 
 textSize(15)
 fill(255)  
@@ -257,11 +282,217 @@ else if(turn == 2){
 }
 
 
+if(block1 == 1 && block2 == 1 && block3 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(670, 310, 1150, 310)
+  bluewon = 1
+  ongoingGame = 0
+}
 
+if(block4 == 1 && block5 == 1 && block6 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(670, 510, 1150, 510)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+if(block7 == 1 && block8 == 1 && block9 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(670, 710, 1150, 710)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+if(block1 == 1 && block4 == 1 && block7 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(710, 300, 710, 710)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+if(block2 == 1 && block5 == 1 && block8 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(910, 300, 910, 710)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+if(block3 == 1 && block6 == 1 && block9 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(1110, 300, 1110, 710)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+if(block1 == 1 && block5 == 1 && block9 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(710, 310, 1110, 710)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+if(block3 == 1 && block5 == 1 && block7 == 1){
+  stroke(0)
+  strokeWeight(20)
+  line(1110, 310, 710, 710)
+  bluewon = 1
+  ongoingGame = 0
+}
+
+// RED WON
+
+if(block1 == 2 && block2 == 2 && block3 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(670, 310, 1150, 310)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block4 == 2 && block5 == 2 && block6 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(670, 510, 1150, 510)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block7 == 2 && block8 == 2 && block9 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(670, 710, 1150, 710)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block1 == 2 && block4 == 2 && block7 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(710, 300, 710, 710)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block2 == 2 && block5 == 2 && block8 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(910, 300, 910, 710)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block3 == 2 && block6 == 2 && block9 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(1110, 300, 1110, 710)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block1 == 2 && block5 == 2 && block9 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(710, 310, 1110, 710)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block3 == 2 && block5 == 2 && block7 == 2){
+  stroke(0)
+  strokeWeight(20)
+  line(1110, 310, 710, 710)
+  redwon = 1
+  ongoingGame = 0
+}
+
+if(block1 != 0 && block2 != 0 && block3 != 0, block4 != 0 && block5 != 0 && block6 != 0, block7 != 0 && block8 != 0 && block9 != 0 && bluewon == 0, redwon == 0){
+  stroke(175)
+  strokeWeight(10)
+  fill(255)
+  rect(80, 100, 450, 800 )
+  stroke(0)
+  textSize(45)
+  strokeWeight(2)
+  fill(0)
+  text("Draw", 105,155)
+  turn = 0
+}
+
+
+if(redwon == 1){
+  stroke(255,0,0)
+  strokeWeight(10)
+  fill(255)
+  rect(80, 100, 450, 800 )
+  stroke(0)
+  textSize(45)
+  strokeWeight(2)
+  fill(0)
+  text("Red won the match!", 105,155)
+  turn = 2
+}
+
+if(bluewon == 1){
+  stroke(0,0,255)
+  strokeWeight(10)
+  fill(255)
+  rect(80, 100, 450, 800 )
+  stroke(0)
+  textSize(45)
+  strokeWeight(2)
+  fill(0)
+  text("Blue won the match!", 105,255)
+  turn = 1
+}
+
+if(ongoingGame == 0){
+  stroke(0)
+  strokeWeight(5)
+  fill(0, playAgainColor, 0)
+  rect(180, 500, 250, 110) // Try Again Button
+  fill(0)
+  strokeWeight(1)
+  text("Play Again!", 190, 570)
+
+  strokeWeight(5)
+  fill(quitColor,0,0)
+  rect(200, 650, 210, 100) // Quit Button
+  fill(0)
+  strokeWeight(1)
+  textSize(40)
+  text("Quit Game", 210, 710)
+}
+
+if (mouseX > 180 && mouseX < 430 && mouseY > 500 && mouseY < 610) {
+    playAgainColor = 255
+}
+else{playAgainColor = 200}
+
+if (mouseX > 200 && mouseX < 410 && mouseY > 650 && mouseY < 750) {
+    quitColor = 255
+}
+else{quitColor = 200}
+
+
+
+
+
+
+
+textSize(15)
+text(turn, 200, 50)
 }
 
 
 function mouseClicked(){
+  if(ongoingGame == 1){
 if (b1c == 200 && block1 == 0) {
   if(turn == 1){
     block1 = 1
@@ -363,6 +594,36 @@ if (b7c == 200 && block7 == 0) {
   else{
     block9 = 2
     turn = 1
+      }
+    }
   }
+if(quitColor == 255){
+homeScreen = 1
 }
+
+if(playAgainColor == 255){
+  ongoingGame = 1
+  bluewon = 0
+  redwon = 0
+  block1 = 0
+  block2 = 0
+  block3 = 0
+  block4 = 0
+  block5 = 0
+  block6 = 0
+  block7 = 0
+  block8 = 0
+  block9 = 0
+  
+}
+
+}
+
+function keyPressed() {
+  if(key === "m" && turn == 2){
+    turn = 1
+  }
+  else if(key === "m"){
+  turn = 2
+  }
 }
